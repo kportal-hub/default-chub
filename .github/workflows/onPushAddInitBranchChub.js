7bd77ba11abec31c73db0c8e45d3fb9eba9db10c
kportal-hub <unclecode@kplay.team>
2020-01-02 11:43:39 +0800

    Update onPushAddInitBranchChub.js

--- .github/workflows/onPushAddInitBranchChub.js
+++ .github/workflows/onPushAddInitBranchChub.js
@@ -193,7 +193,7 @@ async function addActions(branch, username, cube, masterToken, studentToken, cHu
             ref: branch + "-actions"
         })).data;
 
-        let cHubFiles = d.filter(f => !f.name.endsWith(".gitignore") && !f.name.startsWith("onPushLesson")).map(f => f.name);
+        let cHubFiles = d.filter(f => !f.name.endsWith(".gitignore") && !f.name.startsWith("onPushLesson") && !f.name.startsWith("pushTestResult")).map(f => f.name);
         let studentFiles = d.filter(f => !f.name.endsWith(".gitignore") && (f.name.startsWith("onPushLesson") || f.name.startsWith("pushTestResult"))).map(f => f.name);
 
         for (let idx = 0; idx < cHubFiles.length; idx++) {
